dc62aa13d835a7f8bd4d64a5af6f97a5158d5c69
NikitaRy <ryzhakov.h@ya.ru>
2022-09-28 13:25:29 +0300

    Welcome to the Future!

--- github_homework.py
+++ github_homework.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math #библиотека для математики
-import numpy #чисто для массива
-import matplotlib.pyplot as mpp #графики
+import math  # библиотека для математики
+import numpy  # тулза для серьезной математики ;)
+import matplotlib.pyplot as mpp  # либа для построения графика
 
-if __name__=='__main__':
-    arguments = numpy.arange(0, 200, 0.1) #график
+if __name__ == '__main__':  # запускаем программу
+    arguments = numpy.arange(0, 200, 0.1)  # массив для графика
     mpp.plot(
         arguments,
-        [math.cos(a) * math.sin(math.sin(a**2/20.0)) for a in arguments] #функция
+        [math.cos(a) * math.sin(math.sin(a**2/20.0)) for a in arguments]  # график функции внутри
     )
-    mpp.show() #показываем
+    mpp.show()  # показываем график
