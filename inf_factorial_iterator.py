164f16da96d545151735d4f1cb53c106734099d6
Nikita Ryzhakov <43493666+NikitaRy@users.noreply.github.com>
2022-12-18 13:53:09 +0300

    Update and rename Inf_factorial_iterator.py to inf_factorial_iterator.py

--- inf_factorial_iterator.py
+++ inf_factorial_iterator.py
@@ -1,24 +1,26 @@
 import itertools
 
-first_factorials = [1, 1]
+first_factorial = 1
+second_factorial = 1
 
 
 class Factorialinf:
     class _Factorialinf_iter:
         def __init__(self):
             self.i = 0
-            self.factorials = first_factorials
+            self.factorial1 = first_factorial
+            self.factorial2 = second_factorial
 
         def Factorials_generate(self):
             # нам достаточно первые два значения факториала, чтобы далее генирировать
-            self.factorials.append(self.factorials[-1] * self.i)
+            self.factorial2 = self.factorial2 * self.i
 
         def __next__(self):
             j = self.i
             self.i += 1
             if self.i >= 2:
                 self.Factorials_generate()  # собственно: зачем нам лишние числа в начале массива с факториалами?
-            return self.factorials[j]
+            return self.factorial2
 
     def __iter__(self):
         return Factorialinf._Factorialinf_iter()
